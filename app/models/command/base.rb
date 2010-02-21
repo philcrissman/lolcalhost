51e7d38f5576a38ea19a43d6e317d55c3ea9febe
Dmitry Ratnikov <ratnikov@gmail.com>
2010-02-21 16:58:32 -0600

    Added support for prompt and output for commands.

--- app/models/command/base.rb
+++ app/models/command/base.rb
@@ -1,11 +1,17 @@
 class Command::Base
   attr_reader :args
 
+  attr_reader :output, :prompt
+
   def initialize(args = [])
     @args = args
   end
 
   def self.inherited(subclass)
     Command.register subclass
+  end
+
+  def to_json
+    { :output => output, :prompt => prompt }.to_json
   end
 end
--- test/unit/command/base_test.rb
+++ test/unit/command/base_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Commands::BaseTest < ActiveSupport::TestCase
+class Command::BaseTest < ActiveSupport::TestCase
   should("pass truthitest") { assert true }
 
   should("register subclassese as commands") do
@@ -9,5 +9,22 @@ class Commands::BaseTest < ActiveSupport::TestCase
     end
 
     assert Command.defined?('special_command'), "Should have defined the custom 'special_command' command. Space: #{Command.command_space.inspect}"
+  end
+
+  context "#to_json" do
+    should("include command's output") do
+      stub( command = Command::Base.new).output.returns 'Hello world'
+      assert_not_nil(json = ActiveSupport::JSON.decode(command.to_json), "Expeting some json")
+
+      assert_equal 'Hello world', json['output'], "Wrong output. JSON: #{json.inspect}"
+    end
+
+    should("include command's prompt") do
+      stub( command = Command::Base.new ).prompt.returns 'Enter stuff here:'
+
+      assert_not_nil(json = ActiveSupport::JSON.decode(command.to_json), "Expecting json")
+
+      assert_equal 'Enter stuff here:', json['prompt'], "Wrong prompt. JSON: #{json.inspect}"
+    end
   end
 end
